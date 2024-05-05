25b091bbcb9acb792a39d40fc88348f078cd8409
haroldgricejr <gstudio@HaroldsacStudio.attlocal.net>
2024-05-05 14:58:05 -0400

    Code clean up

--- screens/DirectoryPhotosScreen.js
+++ screens/DirectoryPhotosScreen.js
@@ -1,5 +1,3 @@
-// Original working Directory code for list with avatar, title, and description layout
-
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
@@ -36,41 +34,3 @@ const DirectoryScreen = ({ navigation }) => {
 };
 
 export default DirectoryScreen;
-
-
-
-
-
-// import { useState } from 'react';
-// import { FlatList } from 'react-native';
-// import { Avatar, ListItem } from 'react-native-elements';
-// import { PROJECTS } from '../shared/ photoProjects';
-// import { Image } from 'react-native-elements';
-// import { View } from 'react-native';
-// import { StyleSheet } from 'react-native';
-
-// const DirectoryScreen = ({ navigation }) => {
-//     const [photoProjects, setPhotoProjects] = useState(PROJECTS);
-
-//     const renderDirectoryItem = ({ item: photoProject }) => {
-//         return (
-//             <ListItem>
-//                 <View>
-//                     <Image 
-//                         style={{ width: 250, height: 250 }}
-//                         source={photoProject.icon}  
-//                         />
-//                 </View>
-//             </ListItem>
-//         );
-//     };
-//     return (
-//         <FlatList
-//             data={photoProjects}
-//             renderItem={renderDirectoryItem}
-//             keyExtractor={(item) => item.id.toString()}
-//         />
-//     );
-// };
-
-// export default DirectoryScreen;
